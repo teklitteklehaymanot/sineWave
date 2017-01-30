0e997c7b4b591146fe417611a108f4bf074a578e
titi <teklit@insa.et>
2017-01-30 09:41:02 -0800

    ading the project in the first time

---
+++ sinWave/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace sinWave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.picBox.Location = new System.Drawing.Point(3, 38);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(900, 513);
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            this.picBox.Click += new System.EventHandler(this.picBox_Click);
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(78, 12);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 1;
+            this.txtTime.Text = "900";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(184, 12);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "time/length";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 556);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.picBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ sinWave/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace sinWave
+{
+    public partial class Form1 : Form
+    {
+        float[] buffer = new float[8000];
+        float halfY;
+        float amplitude = 100;
+        float frequency = 50;
+        float x1 = 0;
+        float y1 = 0;
+        float t;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(ExecutePlay);
+            t.Start();
+        }
+        public void ExecutePlay()
+        {
+            Graphics DrawWave = picBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.Red, 2);
+            t = Convert.ToInt32(txtTime.Text);// time or length 
+            halfY = picBox.Height / 2;
+            float nextDraw = 0;
+            x1 = 0;
+            y1 = 0;
+            
+        Drowagain:
+            
+            for (int x=0; x < t; x ++)
+                {
+                if(x > 900)
+                {
+                    break;
+                }
+                buffer[x] = (float)(amplitude * Math.Sin(2 * Math.PI * frequency * ((x + nextDraw) / 11025)));
+                DrawWave.DrawLine(pen, (x1), (y1 + halfY), (x), (buffer[x] + halfY));// draw sin wave
+                x1 = x;
+                y1 = buffer[x];
+                }
+
+            x1 = 0;
+            nextDraw = nextDraw + 900;
+            float rest = t - 900;
+            t = rest;
+            if (t > 0)
+            {
+                DrawWave.Clear(Color.Black);
+                goto Drowagain;
+            }
+        }
+
+        private void picBox_Click(object sender, EventArgs e)
+        {
+                Point point = picBox.PointToClient(Cursor.Position);
+                float x = point.X - 100;
+                float y = point.Y - 100;
+                float X = x + 290;
+
+                // draw rectangle
+                Graphics DrawWave = picBox.CreateGraphics();
+                Pen penzoom = new Pen(Brushes.White, 3);
+                DrawWave.DrawRectangle(penzoom, x, y, 300, 300);
+                //DrawWave.DrawRectangle(penzoom, x, 0, 300, 550);
+                //DrawWave.Clear(Color.Black);
+                DrawWave.DrawRectangle(penzoom, 1, 1, 898, 510);
+                x1 = 0;
+                y1 = 0;
+                float y2 = 0;
+                for (int j = 0; j < 1000; j++)
+                {
+                    if (x > X)
+                    {
+                        break;
+                    }
+                    y2 = (float)(amplitude * Math.Sin(2 * Math.PI * frequency * (x / 11025)));
+                    DrawWave.DrawLine(penzoom, (x1 * 3.08f), ((y1 * 2) + halfY), (j * 3.08f), ((y2 * 2) + halfY));
+                    x1 = j;
+                    y1 = y2;
+                    x++;
+                }
+        }
+    }
+}
+
